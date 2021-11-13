6305fa7e2bc2b300c7fea63717387609a11f8bf1
pragat-pandya <pragat.pandya@gmail.com>
2021-11-13 15:46:04 +0530

    Set up URL for fanbook in layout, and create new template for fanbook

--- app/views.py
+++ app/views.py
@@ -2,5 +2,12 @@ from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+
+# Landing page of the website.
 def index (request):
+    # Further cookie based behaviour to be coded here.
     return render(request, "app/index.html")
+
+# Fanbook View
+def fanbook (request):
+    return render (request, "app/fanbook.html")
